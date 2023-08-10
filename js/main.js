335a46dfa081f84167a0957dd5256608e3b41fa1
www.sun Stock.ir <mr.sefroyek@gmail.com>
2023-08-11 02:09:03 +0330

    Add menu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    let menu=document.querySelector(".menu");
+    menu.style.display = 'flex'
+}
+function hideMenu(){
+    let menu=document.querySelector(".menu");
+    menu.style.display = 'none'
+}
