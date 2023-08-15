8c277057591cf01a17cd3c9bff0d27b2b5580fc9
www.sun Stock.ir <mr.sefroyek@gmail.com>
2023-08-15 23:51:59 +0330

    Create about me

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@ function hideMenu(){
     let menu=document.querySelector(".menu");
     menu.style.display = 'none'
 }
+function goTo(section){
+    let sections=document.querySelectorAll(".section");
+    for (let i = 0; i < sections.length; i++) {
+        sections[i].classList.remove("show");
+        sections[i].classList.add("hide");
+    }
+    let item=document.querySelector(`.${section}`);
+    item.classList.add("show");
+    this.hideMenu();
+}
