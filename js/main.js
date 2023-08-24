4cd70baf0955a81475eb1ba925b4bbedaff540c0
www.sun Stock.ir <mr.sefroyek@gmail.com>
2023-08-25 02:56:05 +0330

    rewrite menu

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
-function showMenu(){
-    let menu=document.querySelector(".menu");
-    menu.style.display = 'flex'
+
+function toggleMenu(){
+    let menuIcon=document.querySelector(".menu-icon");
+    let menuContent=document.querySelector(".menu-content");
+    if(menuIcon.hasAttribute("open-mode")){
+      menuIcon.classList.add("bi-x");
+      menuIcon.classList.remove("bi-list");
+      menuIcon.removeAttribute("open-mode");
+      menuContent.style.display = 'flex';
+    }else {
+        menuIcon.classList.add("bi-list");
+        menuIcon.classList.remove("bi-x"); 
+        menuIcon.setAttribute("open-mode","true"); 
+        menuContent.style.display = 'none'; 
+    }
+   
 }
-function hideMenu(){
-    let menu=document.querySelector(".menu");
-    menu.style.display = 'none'
-}
-function goTo(section){
+function showSection(sectionName){
     let sections=document.querySelectorAll(".section");
     for (let i = 0; i < sections.length; i++) {
         sections[i].classList.remove("show");
         sections[i].classList.add("hide");
     }
-    let item=document.querySelector(`.${section}`);
+    let item=document.querySelector(`.${sectionName}`);
     item.classList.add("show");
-    this.hideMenu();
 }
